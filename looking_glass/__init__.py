01e165b54cb6514715dc2e3c9262a4f220cbfde7
Taylor-Jayde Blackstone <tayjaybabee@gmail.com>
2020-02-02 21:29:41 -0600

    Added more code, removed some stuff

--- looking_glass/__init__.py
+++ looking_glass/__init__.py
@@ -1,1 +1,21 @@
-im
+def configure():
+    from .lib.common import conf
+
+
+def start_logger(quiet=False, dev=False, verbose=False, gui=True):
+    from .lib.common import logger
+    from .lib.common.logger import InvalidLevelError
+    import logging
+    level = ''
+    if dev:
+        level = 'DEBUG'
+    elif verbose:
+        level = 'INFO'
+    elif quiet:
+        level = 'CRITICAL'
+
+    try:
+        log = logger.start(__name__, level=level)
+    except InvalidLevelError:
+        log = logging.getLogger()
+        log.fatal('Invalid log level provided')
